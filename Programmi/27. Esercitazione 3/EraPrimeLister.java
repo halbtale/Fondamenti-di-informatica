8a0be499e12d5873ddc5756759418122bbdc71ef
Alberto <42894888+Nenino2@users.noreply.github.com>
2023-11-18 16:47:29 +0100

    era prime lister

--- Programmi/27. Esercitazione 3/EraPrimeLister.java
+++ Programmi/27. Esercitazione 3/EraPrimeLister.java
@@ -18,9 +18,24 @@ public class EraPrimeLister {
         }
 
         for (int i = 0; i < numbers.length; i++) {
-            int currentNumber = numbers[i];
-
+            if (numbers[i] >= 2) {
+                for (int j = 0; j < numbers.length; j++) {
+                    if (numbers[j] % numbers[i] == 0 && numbers[j] != numbers[i]) {
+                        numbers[j] = 0;
+                    }
+                }
+            }
         }
+
+        String result = "numeri primi nell'intervallo [1," + n + "]: ";
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] != 0) {
+                result += i + " ";
+            }
+        }
+
+        System.out.println(result);
 
         in.close();
     }
