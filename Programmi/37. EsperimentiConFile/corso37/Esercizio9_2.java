4a8b3b6443fa96a53f5102ea18bd243246f97e31
Alberto <42894888+Nenino2@users.noreply.github.com>
2023-12-16 12:35:23 +0100

    Added file example

---
+++ Programmi/37. EsperimentiConFile/corso37/Esercizio9_2.java
@@ -0,0 +1,41 @@
+package corso37;
+
+import java.util.Scanner;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Esercizio9_2 {
+    public static void main(String[] args) {
+        Scanner tastiera = new Scanner(System.in);
+        String testoInput;
+        String testoOutput;
+        String reversedText = "";
+
+        System.out.println("Inserire il nome del file txt da importare: ");
+        testoInput = tastiera.nextLine();
+        System.out.println("Inserire il nome del file txt dove stampare il contenuto: ");
+        testoOutput = tastiera.nextLine();
+        tastiera.close();
+
+        try (FileReader reader = new FileReader(testoInput); Scanner scan = new Scanner(reader)) {
+            while (scan.hasNextLine()) {
+                String line = scan.nextLine();
+                String reverseLine = "";
+                for (int i = 0; i < line.length(); i++) {
+                    reverseLine = line.charAt(i) + reverseLine;
+                }
+                reversedText = reversedText + reverseLine + "\n";
+            }
+            try (FileWriter writer = new FileWriter(testoOutput)) {
+                writer.write(reversedText);
+                writer.close();
+            } catch (IOException e) {
+                System.out.println("ERRORE: file in uscita");
+            }
+        } catch (IOException e) {
+            System.out.println("Errore in lettura delle parole");
+        }
+
+    }
+}
