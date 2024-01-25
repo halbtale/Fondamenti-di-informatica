a39efec75016f39a84be8ff881088172b237959c
Alberto <42894888+Nenino2@users.noreply.github.com>
2024-01-25 08:50:54 +0100

    Added

--- Esercitazione esami/Esame20230223/B1/Sequenze.java
+++ Esercitazione esami/Esame20230223/B1/Sequenze.java
@@ -1,7 +1,7 @@
-// Nome:
-// Cognome:
-// Matricola: 
-// Postazione: 
+// Nome: Alberto 
+// Cognome: Heissl 
+// Matricola: 2101739
+// Postazione: 10
 // 23 Febbraio 2023
 // MI RITIRO: NO
 
@@ -10,8 +10,41 @@ public class Sequenze
 {  
     public static void main(String[] args)
     {
-        
-       ...
+        if (args.length != 2) {
+            System.err.println("Uso: java Sequenze [stringa1] [stringa2]");
+            System.exit(1);
+        }
+
+        String stringa1 = args[0];
+        String stringa2 = args[1];
+
+        int foundI = -1;
+        int foundJ = -1;
+        int foundN = 0;
+        String foundSubstring = "";
+
+        for (int i = 0; i<stringa1.length(); i++) {
+            for (int j = 0; j<stringa2.length(); j++) {
+                for (int n = 1; n<Math.min(stringa1.length() - i, stringa2.length() - j); n++) {
+                    if (stringa1.substring(i, i+n).equals(stringa2.substring(j, j+n))) {
+                        if (n > foundN) {
+                            foundI = i;
+                            foundJ = j;
+                            foundN = n;
+                            foundSubstring = stringa1.substring(i, i+n);
+                        }
+                    }
+                }
+            }
+        }
+
+        if (foundN == 0) {
+            System.out.println("Nessuna sottostringa in comune trovata");
+        } else {
+            System.out.println("Sottostringa trovata: "+foundSubstring);
+            System.out.println("Indice iniziale in s1: "+foundI);
+            System.out.println("Indice iniziale in s2: "+foundJ);
+        }
 
     }
 }
--- Esercitazione esami/Esame20230223/B2/RimuoveDoppioni.java
+++ Esercitazione esami/Esame20230223/B2/RimuoveDoppioni.java
@@ -1,7 +1,7 @@
-// Nome:
-// Cognome: 
-// Matricola:
-// Postazione:
+// Nome: Alberto
+// Cognome: Heissl
+// Matricola: 2101739
+// Postazione: 10
 // 23 Febbraio 2023
 // MI RITIRO DALL'ESAME: NO
 
@@ -9,8 +9,70 @@
 import java.util.Scanner;
 
 public class RimuoveDoppioni
-{   public static void main(String[] args)
-    {   
-        ...
+{   
+    private static final String ANSI_GREEN = "\u001B[32m";
+    private static final String ANSI_YELLOW = "\u001B[33m";
+    private static final String ANSI_RESET = "\u001B[0m";
+
+    public static void main(String[] args) {   
+        Scanner in = new Scanner(System.in);
+        Stack s = new ArrayStack();
+
+        while (in.hasNext()) {
+            try {
+                int n = Integer.parseInt(in.next());
+                s.push(n);
+            } catch (NumberFormatException e) {
+                System.err.println(ANSI_YELLOW + "Formato del numero inserito non valido" + ANSI_RESET);
+            }
+        }
+
+        in.close();
+
+        removeDuplicates(s);
+
+        if (s.isEmpty()) {
+            System.err.println(ANSI_YELLOW + "La pila inserita e' vuota" + ANSI_RESET);
+        } else {
+            System.out.println(ANSI_GREEN + "Il risultato è: " + ANSI_RESET);
+            while (!s.isEmpty()) {
+                System.out.println(s.pop());
+            }
+        }
     }  
+
+    private static void removeDuplicates(Stack s) {
+        Stack tempS = new ArrayStack();
+
+        int elementToCheckIndex = 0;
+        boolean shouldLoop = true;
+
+        while (shouldLoop) {
+            Object elementToCheck = null;
+            int i = 0;
+
+            while (!s.isEmpty()) {
+                Object currentElement = s.pop();
+                if (i == elementToCheckIndex) {
+                    elementToCheck = currentElement;
+                    tempS.push(currentElement);
+                }
+                if (elementToCheck == null || !elementToCheck.equals(currentElement)) {
+                    tempS.push(currentElement);
+                }
+
+                i++;
+            }
+
+            if (elementToCheck == null) {
+                shouldLoop = false;
+            }
+
+            while (!tempS.isEmpty()) {
+                s.push(tempS.pop());
+            }
+
+            elementToCheckIndex++;
+        }
+    }
 }
