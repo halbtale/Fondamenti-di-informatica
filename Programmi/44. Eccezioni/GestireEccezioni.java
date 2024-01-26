ee10b7bafda11e42f9e9bdd48c67c34a30184216
Alberto <42894888+Nenino2@users.noreply.github.com>
2024-01-26 15:27:45 +0100

    Added

---
+++ Programmi/44. Eccezioni/GestireEccezioni.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class GestireEccezioni {
+    public static void main(String[] args) {
+        try (Scanner scan = new Scanner(System.in)) {
+            String test = scan.nextLine();
+        } 
+    }
+}
